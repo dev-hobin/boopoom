05d1040389e28795d22896c6176f231d5d28f902
Hobin <66239735+dev-hobin@users.noreply.github.com>
2025-06-28 17:37:07 +0900

    remove: 필요 없어진 버튼 패키지 삭제

--- packages/ui/button/src/button.tsx
+++
@@ -1,9 +0,0 @@
-import { ComponentPropsWithoutRef, forwardRef } from 'react'
-
-export interface ButtonProps extends ComponentPropsWithoutRef<'button'> {}
-
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  (props, ref) => {
-    return <button {...props} ref={ref} />
-  },
-)
