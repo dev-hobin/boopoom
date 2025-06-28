bda6b0537d6a4c88f838bc525c7943b5e85b0b44
Hobin <66239735+dev-hobin@users.noreply.github.com>
2025-06-28 17:21:34 +0900

    feat: 아코디언 스토리북 기본 세팅

---
+++ packages/app/src/stories/Accordion.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import * as Accordion from '@boopoom/accordion'
+
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+const meta = {
+  title: 'UI/Accordion',
+  component: Accordion.Root,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+  args: {},
+} satisfies Meta<typeof Accordion.Root>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    children: ['React', 'Solid', 'Vue', 'Svelte'].map((item) => [
+      <Accordion.Item>
+        <Accordion.ItemTrigger>
+          {item}
+          <Accordion.ItemIndicator>ItemIndicator</Accordion.ItemIndicator>
+        </Accordion.ItemTrigger>
+        <Accordion.ItemPanel>{item} Panel</Accordion.ItemPanel>
+      </Accordion.Item>,
+    ]),
+  },
+}
